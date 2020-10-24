6cda7da7e4348a7b82c3248a8c5494c4598c63c6
Lorraine Makuyana <lorrainemakuyana23@gmail.com>
2020-10-24 07:27:58 +0000

    Edited contact form

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My Personal Portfolio
 
-This is a simple, responsive portfolio created to showcase my skills, projects,  educational qualifications and experiences as well. 
+This is a simple, responsive portfolio created to showcase my skills, projects,  educational qualifications and experiences. 
 
 
 
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+const submitForm = document.querySelector("#submit-form");
+const thankYouDiv = document.querySelector("#thank-you");
+const myForm = document.querySelector("#contact");
+
 function onSubmit(event) {
     event.preventDefault(); 
-    const name = form.elements["full-name"].value;
-    const email = form.elements["email-address"].value;
-    const message = form.elements["message"].value;
-    document.getElementById("thank-you").innerHTML = "";
-    if (name == "" || email == "" || message == "") {
-        document.getElementById("thank-you").innerHTML = "Please complete all form fields.";
+    const name = document.forms["myForm"]["full-name"].value;
+    const email = document.forms["myForm"]["email-address"].value;
+    const message = myForm.elements["message"].value;
+    thankYouDiv.innerHTML = "";
+    if (name !== "" && email !== "" && message !== "") {
+        myForm.style = "display: none; height: 0; overflow: hidden;"; 
+        document.forms["myForm"]["full-name"].value = null;
+        document.forms["myForm"]["email-address"].value = null;
+        myForm.elements["message"].value = null;
+        thankYouDiv.style = "display: block; overflow: visible; padding: 20px 0 0 20px;"; 
+        thankYouDiv.innerHTML = "Thank you for reaching out. You will hear from me soon. <button type=\"submit\" id=\"back-to-form\">Back to form</button>";
     }
-    else {
-        document.getElementById("thank-you").innerHTML = "Thank you for reaching out. You will hear from me soon.";
-    }
+    const backButton = document.querySelector("#back-to-form");
+    backButton.addEventListener("click", backToForm); 
 }
 
-form.addEventListener("submit", onSubmit);
+function backToForm(event) {
+    event.preventDefault(); 
+    thankYouDiv.innerHTML = "";
+    thankYouDiv.style = "display: none; overflow: hidden; height: inherit; width: inherit;";
+    myForm.style = "display: block; padding: 40px 0 0 60px;";
+    console.log("Here"); 
+}
+ 
+submitForm.addEventListener("click", onSubmit); 
